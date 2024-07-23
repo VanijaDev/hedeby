7c2a1616260b340fd1ceaa3057523b97af7ef948
Ivan Solomichev <ivan@ethos.network>
2024-07-23 18:09:19 -0500

    Create warehouse locations

--- contracts/WarehouseLocations.sol
+++ contracts/WarehouseLocations.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
-import { BarcodeCatalog } from "./BarcodeCatalog.sol";
+import { ProductBarcodeCatalog } from "./ProductBarcodeCatalog.sol";
 
-contract WarehouseLocations is BarcodeCatalog { }
+contract WarehouseLocations is ProductBarcodeCatalog { }
